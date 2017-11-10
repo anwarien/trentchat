1f160652813b3fcb680296986cb638333721ab9d
igknighton <trent.knighton@gmail.com>
2017-11-10 12:05:57 -0600

    added User entity and Doctrine Database Migrations Bundle;

--- src/AppBundle/Controller/ChatController.php
+++ src/AppBundle/Controller/ChatController.php
@@ -9,14 +9,60 @@
 namespace AppBundle\Controller;
 
 
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 
 class ChatController extends Controller
 {
+    /**
+     * @Route("/chat/newuser", name="newuser" )
+     */
+    public function createUserAction() {
 
+        $user = new User();
+        $username= 'user'.rand(1,100);
+        $user->setName($username);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        return new Response('<html><body>User ' . $username .' created!</body></html>');
+    }
+
+    /**
+     * @Route("/chat")
+     */
+    public function listUsersAction() {
+
+        $em = $this->getDoctrine()->getManager();
+        $users = $em->getRepository('AppBundle:User')->findAll();
+
+
+        return $this->render('chat.list.html.twig', [
+            'users' => $users,
+            ]);
+    }
+
+    /**
+     * @Route("/chat/notes", name="notes")
+     * @Method("GET")
+     */
+    public function chatNotesAction(){
+
+        $userData = [
+            ['id'=>1,'username' =>'john123'],
+            ['id'=>2,'username' =>'doggyboi3'],
+            ['id'=>3,'username' =>'21igk'],
+        ];
+
+        return new JsonResponse($userData);
+
+    }
     /**
      * @Route("/chat/{chatroom}")
      */
@@ -25,6 +71,8 @@ class ChatController extends Controller
         $template = $this->container->get('templating');
         $html = $template->render('chat/chat.html.twig',
             ['chat'=> $chatroom]);
+        $cache = $this->get('doctrine_cache.providers.my_cache');
+
         return new Response($html);
     }
 }
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Trent
+ * Date: 11/9/2017
+ * Time: 2:43 PM
+ */
+namespace AppBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+
+
+}
